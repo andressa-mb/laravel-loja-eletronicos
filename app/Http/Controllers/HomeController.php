c64ea2c3a2791c24fee3541091121b0cb70430b1
andressa-mb <andressamesquita26@gmail.com>
2025-05-06 16:28:04 -0300

    Ordenação de produtos na tela do buyer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -26,11 +27,29 @@ class HomeController extends Controller
         return view('indexAdm');
     }
 
-    public function indexBuyer(){
-        return view('indexBuyer');
+    public function indexBuyer(Request $request, Product $product){
+        $valueToSort = $request->sort;
+        if(!is_null($valueToSort)){
+            $valueToSort = $request->validate([
+                'sort' => 'required|in:popular,lowest_price,highest_price,recent'
+            ]);
+            $value = $valueToSort['sort'];
+            if($value == 'lowest_price'){
+                $products = $product->orderBy('total', 'asc')->get();
+            } else if($value == 'highest_price'){
+                $products = $product->orderBy('total', 'desc')->get();
+            } else if($value == 'recent'){
+                $products = $product->orderBy('created_at', 'desc')->get();
+            } else {
+                $products = $product->get();
+            }
+            return view('indexBuyer', ['products' => $products, 'sort' => $value]);
+        }else {
+            return view('indexBuyer', ['products' => $product->get()]);
+        }
     }
-    public function indexProfile(Request $request)
-    {
+
+    public function indexProfile(Request $request){
         return view('profile.index', ['user' => $request->user()]);
     }
 }
--- resources/views/indexBuyer.blade.php
+++ resources/views/indexBuyer.blade.php
@@ -1,38 +1,63 @@
 @extends('layouts.app')
 @section('content')
-    <div class="mt-4 row">
+
+
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Buscar produtos..." aria-label="Buscar produtos..." aria-describedby="basic-addon2">
+        <div class="input-group-append">
+        <a href="#"><span class="input-group-text" id="basic-addon2">LUPA</span></a>
+        </div>
+    </div>
+
+    <form action="{{route("index-buyer")}}" method="GET" enctype="multipart/form-data">
+        @csrf
+        <div class="d-flex justify-content-end">
+            <label for="sort" class="mr-2">Ordenar por:</label>
+            <select class="w-25 custom-select custom-select-sm" id="sort" name="sort" onchange="this.form.submit()" required>
+                <option value="popular">Popular</option>
+                <option value="lowest_price">Menor preço</option>
+                <option value="highest_price">Maior preço</option>
+                <option value="recent">Mais recente</option>
+            </select>
+        </div>
+    </form>
+
+    <div class="mt-4 row d-flex">
         <div class="col-md-3 vh-100">
             <nav id="navbar-example3" class="navbar navbar-light bg-light">
                 <a class="navbar-brand" href="#">Categorias</a>
                 <nav class="nav nav-pills flex-column">
                     @foreach (App\Models\Category::get() as $category)
-                    <a class="nav-link" href="#{{$category->id}}">{{$category->name}}</a>
-                    <nav class="nav nav-pills flex-column">
+                        <a class="nav-link" href="#{{$category->id}}">{{$category->name}}</a>
+                        <nav class="nav nav-pills flex-column">
                         @foreach ($category->products as $product)
-                        <a class="nav-link ml-3 my-1" href="#{{$category->id}}">{{$product->name}}</a>
+                            <a class="nav-link ml-3 my-1" href="#{{$category->id}}">{{$product->name}}</a>
                         @endforeach
-                    </nav>
+                        </nav>
                     @endforeach
                 </nav>
                 <a href="{{route('products-associates', $category)}}" class="btn btn-info">Lista de categorias</a>
             </nav>
         </div>
 
-        <div class="col-md-9">
-            @foreach (App\Models\Product::get() as $product)
+        <div class="col-md-9 d-flex flex-wrap">
+            @foreach ($products as $product)
                 @foreach ($product->categories as $category)
-                    <div class="card p-4" style="width: 18rem; margin: 20px auto;">
-                        <div data-spy="scroll" data-target="#navbar-example3" data-offset="0">
-                            <img src="{{asset("storage/{$product->image}")}}" id="{{$category->pivot->category_id}}" class="card-img-top" alt="...">
-                            <h4 class="card-title">{{$product->name}}</h4>
-                            <p class="card-text">{{$product->description}}</p>
-                            <p class="card-text">Quantidade: {{$product->quantity}}</p>
-                            <p class="card-text">R$ {{$product->total}}</p>
-                            <a href="{{route('view-product', $product)}}"><i class="bi bi-eye-fill">  Ver produto</i></a>
+                    <div class="col-md-6">
+                        <div class="card p-4" style="width: 18rem; margin: 20px auto;">
+                            <div data-spy="scroll" data-target="#navbar-example3" data-offset="0">
+                                <img src="{{asset("storage/{$product->image}")}}" id="{{$category->pivot->category_id}}" class="card-img-top" alt="...">
+                                <h4 class="card-title">{{$product->name}}</h4>
+                                <p class="card-text">{{$product->description}}</p>
+                                <p class="card-text">Quantidade: {{$product->quantity}}</p>
+                                <p class="card-text">R$ {{$product->total}}</p>
+                                <a href="{{route('view-product', $product)}}"><i class="bi bi-eye-fill">  Ver produto</i></a>
+                            </div>
                         </div>
                     </div>
                 @endforeach
             @endforeach
         </div>
     </div>
+
 @endsection
