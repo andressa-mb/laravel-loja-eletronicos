4f7461f5a6dc5295eb86f41b1a7f8d6c1a1a906e
andressa-mb <andressamesquita26@gmail.com>
2025-05-14 11:20:40 -0300

    Telas do edit

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="mt-5">
-        <h3>Alteração dos dados:</h3>
-        <form action="{{route('category-update', $category)}}" method="POST" class="content m-auto w-50">
+    <div class="row form-create-bg">
+        <h3 class="col-12 p-2 text-center bg-dark text-white rounded">Alteração dos dados:</h3>
+        <form action="{{route('category-update', $category)}}" method="POST" class="m-auto w-50 form-create">
             @csrf
             @method('PUT')
             <div class="form-group">
-                <label for="name">Nome:</label>
+                <label for="name" class="font-form">Nome:</label>
                 <input type="text" id="name" name="name" class="form-control" value="{{$category->name}}">
             </div>
-            <button type="submit" class="btn btn-success">Salvar</button>
-            <a type="button" class="btn btn-primary" href="{{route('index-adm')}}">Voltar</a>
+            <div class="text-center">
+                <button type="submit" class="btn btn-success">Salvar</button>
+                <a type="button" class="btn btn-primary" href="{{route('index-adm')}}">Voltar</a>
+            </div>
         </form>
 
         @if (session('message'))
--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
 @section('content')
 
-    <div class="mt-5">
-        <h3>Alteração dos dados:</h3>
-        <form action="{{route('product-update', $product)}}" method="POST" enctype="multipart/form-data" class="content m-auto w-50">
+    <div class="row form-create-bg">
+        <h3 class="col-12 p-2 text-center bg-dark text-white rounded">Alteração dos dados:</h3>
+        <form action="{{route('product-update', $product)}}" method="POST" enctype="multipart/form-data" class="m-auto w-50 form-create form-edit">
             @csrf
             @method('PUT')
             <div class="form-group">
-                <label for="name">Nome:</label>
+                <label for="name" class="font-form">Nome:</label>
                 <input type="text" id="name" name="name" class="form-control" value="{{$product->name}}">
-                <label for="description">Descrição:</label>
+                <label for="description" class="font-form">Descrição:</label>
                 <textarea name="description" id="description" cols="18" rows="5" class="form-control">{{$product->description}}</textarea>
-                <label for="price">Valor do produto:</label>
+                <label for="price" class="font-form">Valor do produto:</label>
                 <input type="number" id="price" name="price" step=".01" class="form-control" value="{{$product->price}}">
-                <label for="quantity">Quantidade:</label>
+                <label for="quantity" class="font-form">Quantidade:</label>
                 <input type="number" id="quantity" name="quantity" class="form-control" value="{{$product->quantity}}">
-                <label for="discount">Desconto:</label>
+                <label for="discount" class="font-form">Desconto:</label>
                 <input type="number" step=".01" id="discount" name="discount" class="form-control" value="{{$product->discount}}">
-                <label for="image">Imagem:</label>
+                <label for="image" class="font-form">Imagem:</label>
                 <input type="file" id="image" name="image" class="form-control-file" value="{{$product->image}}">
             </div>
-            <button type="submit" class="btn btn-success">Salvar</button>
-            <a type="button" class="btn btn-primary" href="{{route('index-adm')}}">Voltar</a>
+            <div class="text-center">
+                <button type="submit" class="btn btn-success">Salvar</button>
+                <a type="button" class="btn btn-primary" href="{{route('index-adm')}}">Voltar</a>
+
+            </div>
         </form>
 
         @if (session('message'))
